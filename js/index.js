260e0ad2d54915de101ed591dc507cc271bd1d5d
Alenwuhl <wuhlalen@gmail.com>
2023-07-05 17:08:56 -0300

    Segunda Pre-entrega

--- js/index.js
+++ js/index.js
@@ -1,49 +1,102 @@
-function main() {
+// Defino las constantes
+const prendas = [
+    {id: 1, nombre: "Teddy", precio: 2990, stock: 20},
+    {id: 2, nombre: "Buzo Jogging", precio: 2490, stock: 40},
+    {id: 3, nombre: "Remera XL", precio: 1990, stock: 60}
+]
+const carrito = []
 
-    let exito = true
-    let codigo = elegirProducto();
-    let confirmar = confirm("Confirme si desea realizar la compra");
-    if (confirmar ===true) {
-        mostrarMetodoPago();
-        let cantCuotas = prompt("ingrese la cantidad de cuotas deseadas:");
-        if (cantCuotas > 0 && cantCuotas <=6){
-            exito = mostrarCuotas(codigo, cantCuotas);
-            if (exito){
-                let respuestaComprar = confirm ("Confirme si desea realizar la compra")
-                if (respuestaComprar === true) {
-                    alert ("✔ La prenda ya es tuya!")
-                }
-            }
-        } else {
-            console.error("La cantidad de cuotas debe ser un valor numérico entre 1 y 6")
+// La clase compra tiene la info de la compra que hace el usuario
+class compras {
+    constructor(carritoDeCompras){
+        this.carrito = carritoDeCompras
+        let cantCuotas = 1
+        let importeCadaCuota = 0   // esta propiedad contiene el importe de cada cuota
+    }
+    obtenerSubtotal(){
+        if (this.carrito.length > 0) {
+            return this. carrito.reduce((acc, prenda)=> acc + prenda.precio, 0)
         }
-    } else {
-        console.console.log("😪Que lastima! Esperamos verte otra vez por aquí!");
-    }     
+    }
 }
 
-function mostrarCuotas(codigo, cantidadCuotas) {
+function main() {
+    comprar()
+}
+
+function comprar() {
+    let respuesta = true
+    console.log("Productos disponibles:")
+    console.table(prendas)
+    while (respuesta) {
+        let id = prompt("Ingresa el ID de la prenda que deseas comprar:")
+        let prendaElegida = buscarPrenda(id)
+        console.log(prendaElegida)
+        if (prendaElegida !== undefined) {
+            carrito.push(prendaElegida)
+            alert ("La prenda " + prendaElegida.nombre + " se agregó al carrito.")
+            respuesta = confirm("Deseas seguir comprando?")
+        } else {
+            console.error("No existe el producto con id: " + id)
+        }
+    }
+    if (carrito.length > 0) {
+        console.table (carrito)
+        let confirmar = confirm("Desea confirmar la compra?")
+        if (confirmar ===true) {
+            finalizarCompra()
+        } else {
+            console.log("😪Que lastima! Esperamos verte otra vez por aquí!")
+        }       
+    } else{
+        console.warn("El carrito está vacío")
+    }
+}
+
+function finalizarCompra() {
+    const compra = new compras(carrito)
+
+    mostrarMetodoPago()
+    let cantCuotas = prompt("ingrese la cantidad de cuotas deseadas:");
+    exito = mostrarCuotas(compra, cantCuotas);
+    if (exito){
+        let respuestaComprar = confirm ("Confirme si desea realizar la compra")
+        if (respuestaComprar === true) {
+            alert("El total de tu compras es de $ "+ compra.obtenerSubtotal() + " a pagar en " + compra.cantCuotas + " cuotas de $" + compra.importeCadaCuota + ". Muchas gracias!")
+        } else {
+            alert("😪Que lastima! Esperamos verte otra vez por aquí!")
+        }
+    }
+    
+}
+
+function mostrarMetodoPago() {
+    let intereses = "sin intereses"
+    console.log ("Puedes realizar la compra con los siguientes métodos de pago en cuotas:")
+    for (let i = 1; i <= 6; i++) {
+        if (i > 3) {
+            intereses= "con intereses."
+        }
+        console.log (i + " cuotas(s) " + intereses)
+    }
+}
+
+function mostrarCuotas(compra, cantidadCuotas) {
     let cuota = 0;
     let exito = true;
     if (cantidadCuotas > 0 && cantidadCuotas <= 6){
-        switch (codigo) {
-            case "Teddys":
-                cuota = calcularCuota (2990, cantidadCuotas)
-                break;
-            case "RemerasXL":
-                cuota = calcularCuota (1990, cantidadCuotas)
-                break;
-            case "BuzoJogging":
-                cuota = calcularCuota (2490, cantidadCuotas)
-                break;
-        } 
-        console.log("La compra de " + codigo + " sería en " + cantidadCuotas + " cuotas de $" + cuota)
+        cuota = calcularCuota (compra.obtenerSubtotal(), cantidadCuotas)
+        compra.importeCadaCuota = cuota
+        compra.cantCuotas = cantidadCuotas
+        console.log("La compra será pagada en " + cantidadCuotas + " cuotas de $" + cuota)
     }else {
         exito = false
+        compra.importeCadaCuota = compra.obtenerSubtotal()
+        compra.cantCuotas = 1
         console.error("La cantidad de cuotas debe ser un valor numérico entre 1 y 6")
     }
-    return exito;
-}    
+    return exito;    
+}
 
 function calcularCuota(monto, cantidadCuotas) {
     if (cantidadCuotas > 0 && cantidadCuotas <= 3) {
@@ -55,34 +108,10 @@ function calcularCuota(monto, cantidadCuotas) {
     }
      
 }
-function elegirProducto() {
-    let codigo = prompt("Ingresa la prenda que quieres consultar. Las opciones disponibles por el momento son: Teddys, RemerasXL o BuzoJogging.")
-    precios(codigo)
-    return codigo
+
+function buscarPrenda(id){
+    let resultado = prendas.find((prenda)=> prenda.id === parseInt(id))
+    return resultado
 }
-function precios(codigo) {
-    switch (codigo) {
-        case "Teddys":
-            console.log("Te felicitamos por tu elección! Amamos nustros buzos Teddys. El precio de cada Teddy es: $2990")
-            break;
-        case "RemerasXL":
-            console.log("Te felicitamos por tu elección! Amamos nuestras Remeras XL. El precio de cada remera es de: $1990")
-            break;
-        case "BuzoJogging":
-            console.log("Te felicitamos por tu elección! Amamos nuestros Buzos de jogging. El precio de cada buzo es de: $2490")
-            break;
-        default:
-            console.warn("😕No has elegido ninguna de nuestras prendas. Te recordamos que las opciones disponibles son: Teddys, RemerasXL o BuzoJogging")
-            break;
-    }
-}
-function mostrarMetodoPago() {
-    let intereses = "sin intereses"
-    console.log ("Puedes realizar la compra con los siguientes metodos de pago en cuotas:")
-    for (let i = 1; i <= 6; i++) {
-        if (i > 3) {
-            intereses= "con intereses."
-        }
-        console.log (i + " cuotas(s) " + intereses)
-    }
-}
+
+
